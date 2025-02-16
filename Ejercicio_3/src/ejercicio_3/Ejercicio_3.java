c4cea666bfc85ecb9e4f504538c2b880973cd2d0
johanzcs <sebaszambranoc3@gmail.com>
2025-02-16 16:28:14 -0500

    Add files via upload

---
+++ Ejercicio_3/src/ejercicio_3/Ejercicio_3.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_3;
+
+import java.util.Scanner;
+public class Ejercicio_3 {
+
+   public static void main(String[] args) {
+        Scanner LimitedePeso = new Scanner(System.in); 
+        int pisoActual = 1;
+        int pisoMin = -1;
+        int pisoMax = 13;
+        int pesoMaximo = 300; 
+
+        System.out.println("Bienvenido al ascensor.");
+        
+        while (true) { 
+            System.out.print("Ingresa tu peso en kg: ");
+            int pesoUsuario = LimitedePeso.nextInt();
+
+            if (pesoUsuario > pesoMaximo) {
+                System.out.println("¡Sobrecarga! Peso maximo permitido: " + pesoMaximo + " kg. Intenta de nuevo.");
+                continue; 
+            }
+
+            System.out.println("Estas en el piso " + pisoActual);
+            System.out.print("Ingresa el piso al que deseas ir (entre " + pisoMin + " y " + pisoMax + "): ");
+            
+            int pisoDestino = LimitedePeso.nextInt();
+
+            if (pisoDestino >= pisoMin) { 
+                if (pisoDestino <= pisoMax) { 
+                    System.out.println("Moviendo el ascensor...");
+                    while (pisoActual < pisoDestino) {
+                        pisoActual++;
+                        System.out.println("Subiendo...piso  : " + pisoActual);
+                    }
+                    while (pisoActual > pisoDestino) {
+                        pisoActual--;
+                        System.out.println("Bajando... piso : " + pisoActual);
+                    }
+                    System.out.println("Has llegado al piso " + pisoActual + ". Puertas abiertas.");
+                } else {
+                    System.out.println("Error: Piso fuera de rango. Intenta de nuevo.");
+                }
+            } else {
+                System.out.println("Error: Piso fuera de rango. Intenta de nuevo.");
+            }
+        }
+    }
+}
